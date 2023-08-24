6db8c346076cdc1274d15ea4290857d3b54f5b3b
RIcardo Medina Herrera <99689704+Richar03verysexy@users.noreply.github.com>
2023-08-23 19:08:50 -0500

    Create ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public ejercicio1 Main {
+    public static void main(String[] args) {
+      int num1;
+      int[] list = new int[7];
+      list[0] = 40;
+      list[1] = 50;
+      list[2] = 60;
+      list[3] = 24;
+      list[4] = 66;
+      list[5] = 12;
+      list[6] = 98;
+      num1 = (40+50+60+24+66+12+98)/7;
+      System.out.println(num1);
+      for(int listnum:list){
+        if (listnum==num1){
+          System.out.println("El numero "+listnum+" es igual que el promedio");
+        }else if(listnum<num1){
+          System.out.println("El numero "+listnum+" es menor que el promedio");
+        }
+        else{
+          System.out.println("El numero "+listnum+" es mayor que el promedio");
+        }
+      }
+  }
+}
