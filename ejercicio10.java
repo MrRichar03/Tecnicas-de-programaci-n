76a2972924f2c0ae2491d4745a1c5ab903bf0c50
RIcardo Medina Herrera <99689704+Richar03verysexy@users.noreply.github.com>
2023-08-23 19:10:00 -0500

    Create ejercicio10.java

---
+++ ejercicio10.java
@@ -0,0 +1,26 @@
+import java.util.;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Desarrollo ejercicio 1");
+      System.out.println("");
+      System.out.println("Primero pasaremos de celsius a Fahrenheit ");
+      System.out.println("");
+      System.out.println(" 0C a Fahrenheit es : " + ((09/5) +32));
+      System.out.println("");
+      System.out.println(" 100C a Fahrenheit es : " + ((1009/5) +32));
+      System.out.println("");
+      System.out.println(" 25C a Fahrenheit es : " + ((259/5) +32));
+      System.out.println("");
+      System.out.println(" -10C a Fahrenheit es : " + ((-109/5) +32));
+      System.out.println("");
+      System.out.println("Ahora pasaremos de Fahrenheit a celsius ");
+      System.out.println("");
+      System.out.println(" 32F a celsius es : " + ((32-32)5/9));
+      System.out.println("");
+      System.out.println(" 100F a celsius es : " + ((100-32)5/9));
+      System.out.println("");
+      System.out.println(" -40F a celsius es : " + ((-40-32)5/9));
+
+  }
+}
