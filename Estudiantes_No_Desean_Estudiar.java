58df7603f2f6a7ac67946a9e6b0f2ccec1074448
RIcardo Medina Herrera <99689704+MrRichar03@users.noreply.github.com>
2023-09-15 09:07:37 -0500

    Update Estudiantes_No_Desean_Estudiar.java

--- Estudiantes_No_Desean_Estudiar.java
+++ Estudiantes_No_Desean_Estudiar.java
@@ -134,7 +134,7 @@ public class Estudiantes_No_Desean_Estudiar {
         //Contar los que no tienen recursos y les enseñan de forma antigua y los que no cumplen los tres
         double Problemas_Economicos_Ensenanaza_Antigua =Conteo_Estudiantes_Problemas_Economicos_Esenanzas_Antiguas(estudiantes);
         double Problemas_Economicos_Ensenanaza_Antigua_No_estudiar = Conteo_Estudiantes_Problemas_Economicos_Esenanzas_Antiguas_No_quieren_estudiar(estudiantes);
-        // Contar cuántos estudiantes se les enseña de maera antigua y no desean estudiar 
+        // Contar cuántos estudiantes se les enseña de maera antigua y no desean estudiar
         double Cantidad_Estudiantes_Que_no_les_gusta_estudiar_por_ensenanza_antigua= Conteo_Estudiantes_Que_No_Le_Gusta_Estudiar(estudiantes);
 
         // Calcular la moda de las edades
@@ -146,29 +146,29 @@ public class Estudiantes_No_Desean_Estudiar {
         System.out.println("A continuación se presentarán los resultados de una encuesta realizada jóvenes estudiantes del país");
         System.out.println("El tema de la encuesta es el agrado hacia el estudio y las causas de este");
         System.out.println("");
-        System.out.println("la cantidad de encuestados es " + estudiantes.size());
+        System.out.println("La cantidad de encuestados es " + estudiantes.size());
         System.out.println("La edad promedio de los encuestados es de " + Promedio_De_Edades+" años y la edad que mas se repite es "+ Edad_Moda);
         System.out.println("");
-        System.out.println("La cantidad de estuiantes que se les sigue enseñando de maneras antiguas es: " + Estudiantes_Que_Les_Ensenan_De_Forma_Antigua);
-        System.out.println("La cantidad de estuiantes que se les enseña de maneras modernas es: " + Estudiantes_Que_No_Les_Ensenan_De_Forma_Antigua);
-        System.out.println("La cantidad de estuiantes que tienen problemas economicos es: " + Problemas_Economicos);
+        System.out.println("La cantidad de estudiantes que se les sigue enseñando de maneras antiguas es: " + Estudiantes_Que_Les_Ensenan_De_Forma_Antigua);
+        System.out.println("La cantidad de estudiantes que se les enseña de maneras modernas es: " + Estudiantes_Que_No_Les_Ensenan_De_Forma_Antigua);
+        System.out.println("La cantidad de estudiantes que tienen problemas económicos es: " + Problemas_Economicos);
         System.out.println("");
         System.out.println("En porcentaje podemos observar que: ");
-        System.out.println("El "+ (Problemas_Economicos_Ensenanaza_Antigua/estudiantes.size())*100+"% no tienen recursos economicos y se les enseña de manera antigua");
+        System.out.println("El "+ (Problemas_Economicos_Ensenanaza_Antigua/estudiantes.size())*100+"% no tienen recursos económicos y se les enseña de manera antigua");
         System.out.println("El "+ ((Cantidad_De_Estudiantes_Que_No_Les_Gusta_Estudiar)/estudiantes.size())*100+"% no desean estudiar");
-        System.out.println("El "+ (Cantidad_Estudiantes_No_Continuan_Por_Recursos_Economicos/estudiantes.size())*100+"% no desean estudiar y tienen problemas economicos");
+        System.out.println("El "+ (Cantidad_Estudiantes_No_Continuan_Por_Recursos_Economicos/estudiantes.size())*100+"% no desean estudiar y tienen problemas económicos");
         System.out.println("El "+ (Cantidad_Estudiantes_Que_no_les_gusta_estudiar_por_ensenanza_antigua/estudiantes.size())*100+"% no desean estudiar y se les enseña de manera antigua");
-        System.out.println("El "+ (Problemas_Economicos_Ensenanaza_Antigua_No_estudiar/estudiantes.size())*100+"% no desean estudiar, se les enseña de manera antigua y tienen problemas economicos");
+        System.out.println("El "+ (Problemas_Economicos_Ensenanaza_Antigua_No_estudiar/estudiantes.size())*100+"% no desean estudiar, se les enseña de manera antigua y tienen problemas económicos");
         System.out.println("");
         System.out.println("Con estos resultados podemos decir ");
-        System.out.println("1- La mayoria de estudiantes tienen problemas economicos y se les enseña de manera ortodoxa ");
-        System.out.println("2- La mayoria de estudiantes no desean estudiar y la principal causa es la forma en que se enseña");
-        System.out.println("3- La situación economica tambien es un gran afectante encuanto al disgusto por estudiar ");
+        System.out.println("1- La mayoría de estudiantes tienen problemas económicos y se les enseña de manera ortodoxa ");
+        System.out.println("2- La mayoría de estudiantes no desean estudiar y la principal causa es la forma en que se enseña");
+        System.out.println("3- La situación económica también es un gran causante en cuanto al disgusto por estudiar ");
         System.out.println("");
         System.out.println("En conclusión:");
-        System.out.println("se debe implementar nuevas estrategias para captar la atención de los jovenes");
-        System.out.println("Y se debe realizar ayuda social para mejorar la carente situación economica de los jovenes");
-        
+        System.out.println("Se debe implementar nuevas estrategias para captar la atención de los jóvenes");
+        System.out.println("Y se debe realizar ayuda social para mejorar la carente situación económica de los jóvenes");
+
     }
 
     // Método para calcular el promedio de una lista de edades
@@ -190,7 +190,7 @@ public class Estudiantes_No_Desean_Estudiar {
         }
         return contador;
     }
-    // Método para contar cuántos estudiantes no desean estudiar 
+    // Método para contar cuántos estudiantes no desean estudiar
     private static int Conteo_No_le_gusta_Estudiar(List<Estudiante> estudiantes){
         int contador = 0;
         for (Estudiante estudiante : estudiantes) {
@@ -234,7 +234,7 @@ public class Estudiantes_No_Desean_Estudiar {
         }
         return contador;
     }
-    // Método para contar cuántos estudiantes tienen problemas economicos y se les enseña de manera antigua 
+    // Método para contar cuántos estudiantes tienen problemas economicos y se les enseña de manera antigua
     private static int Conteo_Estudiantes_Problemas_Economicos_Esenanzas_Antiguas(List<Estudiante> estudiantes) {
         int contador = 0;
         for (Estudiante estudiante : estudiantes) {
